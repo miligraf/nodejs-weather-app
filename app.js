8d3eb901606b8db02c3ed18cc4d61e89fe1d1252
Milton Andrade <miligraf@gmail.com>
2018-03-06 14:30:36 -0500

    Refactor and move out geocoding call to own file

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=525%20technology%20park%20lake%20mary%20florida&key=${process.env.GOOGLE_API_KEY}`,
-  json: true
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describre: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
