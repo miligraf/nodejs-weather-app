23ac9bca8c2af823e0384b87130176d6ce422799
Milton Andrade <miligraf@gmail.com>
2018-03-06 13:29:10 -0500

    Print out formatted_address, lat and lng from Google Geocoding API response

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=525%20technology%20park%20lake%20mary%20florida&key=${process.env.GOOGLE_API_KEY}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
